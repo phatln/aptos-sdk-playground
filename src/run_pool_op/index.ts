b653b68757c86f814ae1d8eefdc17c613b27cf3f
phatln <phatln@undercurrent.tech>
2025-06-18 17:05:10 +0700

    run pool op

---
+++ src/run_pool_op/index.ts
@@ -0,0 +1,120 @@
+import { Serializer } from "@aptos-labs/ts-sdk";
+
+class CLMM {
+  private poolAddr: string;
+  private readonly OP_CLMM_RESET_INIT_PRICE: number = 0xa0;
+  private readonly OP_INITIALIZE_POOL_BLACKLIST: number = 0xa1;
+  private readonly OP_BLACKLIST_POSITION: number = 0xa2;
+  private readonly OP_UNBLACKLIST_POSITION: number = 0xa3;
+
+  constructor(poolAddr: string) {
+    this.poolAddr = poolAddr;
+  }
+
+  async resetInitPrice(initPrice: bigint) {
+    const serializer = new Serializer();
+    serializer.serializeU8(this.OP_CLMM_RESET_INIT_PRICE);
+    serializer.serializeU128(initPrice);
+    const data = serializer.toUint8Array();
+    console.log(`run_pool_op: ${this.poolAddr} reset_init_price`);
+    console.log(`args: `, data);
+  }
+
+  async initializePoolBlacklist() {
+    const serializer = new Serializer();
+    serializer.serializeU8(this.OP_INITIALIZE_POOL_BLACKLIST);
+    const data = serializer.toUint8Array();
+    console.log(`run_pool_op: ${this.poolAddr} initialize_pool_blacklist`);
+    console.log(`args: `, data);
+  }
+
+  async blacklistPosition(posId: number) {
+    const serializer = new Serializer();
+    serializer.serializeU8(this.OP_BLACKLIST_POSITION);
+    serializer.serializeU64(posId);
+    const data = serializer.toUint8Array();
+    console.log(`run_pool_op: ${this.poolAddr} blacklist_position`);
+    console.log(`args: `, data);
+  }
+
+  async unblacklistPosition(posId: number) {
+    const serializer = new Serializer();
+    serializer.serializeU8(this.OP_UNBLACKLIST_POSITION);
+    serializer.serializeU64(posId);
+    const data = serializer.toUint8Array();
+    console.log(`run_pool_op: ${this.poolAddr} unblacklist_position`);
+    console.log(`args: `, data);
+  }
+}
+
+class Stable {
+  private poolAddr: string;
+  private readonly OP_STABLE_RAMP_A: number = 0xa0;
+  private readonly OP_STABLE_STOP_RAMP_A: number = 0xa1;
+  private readonly OP_STABLE_SET_NEW_FEE: number = 0xa2;
+  private readonly OP_INITIALIZE_POOL_BLACKLIST: number = 0xa3;
+  private readonly OP_BLACKLIST_POSITION: number = 0xa4;
+  private readonly OP_UNBLACKLIST_POSITION: number = 0xa5;
+
+  constructor(poolAddr: string) {
+    this.poolAddr = poolAddr;
+  }
+
+  async rampA(future_a: bigint, future_time: number) {
+    const serializer = new Serializer();
+    serializer.serializeU8(this.OP_STABLE_RAMP_A);
+    serializer.serializeU256(future_a);
+    serializer.serializeU64(future_time);
+    const data = serializer.toUint8Array();
+    console.log(`run_pool_op: ${this.poolAddr} ramp_a`);
+    console.log(`args: `, data);
+  }
+
+  async stopRampA() {
+    const serializer = new Serializer();
+    serializer.serializeU8(this.OP_STABLE_STOP_RAMP_A);
+    const data = serializer.toUint8Array();
+    console.log(`run_pool_op: ${this.poolAddr} stop_ramp_a`);
+    console.log(`args: `, data);
+  }
+
+  async setNewFee(newFee: bigint, newOffpegFeeMultiplier: bigint) {
+    const serializer = new Serializer();
+    serializer.serializeU8(this.OP_STABLE_SET_NEW_FEE);
+    serializer.serializeU256(newFee);
+    serializer.serializeU256(newOffpegFeeMultiplier);
+    const data = serializer.toUint8Array();
+    console.log(`run_pool_op: ${this.poolAddr} set_new_fee`);
+    console.log(`args: `, data);
+  }
+
+  async initializePoolBlacklist() {
+    const serializer = new Serializer();
+    serializer.serializeU8(this.OP_INITIALIZE_POOL_BLACKLIST);
+    const data = serializer.toUint8Array();
+    console.log(`run_pool_op: ${this.poolAddr} initialize_pool_blacklist`);
+    console.log(`args: `, data);
+  }
+
+  async blacklistPosition(posId: number) {
+    const serializer = new Serializer();
+    serializer.serializeU8(this.OP_BLACKLIST_POSITION);
+    serializer.serializeU64(posId);
+    const data = serializer.toUint8Array();
+    console.log(`run_pool_op: ${this.poolAddr} blacklist_position`);
+    console.log(`args: `, data);
+  }
+
+  async unblacklistPosition(posId: number) {
+    const serializer = new Serializer();
+    serializer.serializeU8(this.OP_UNBLACKLIST_POSITION);
+    serializer.serializeU64(posId);
+    const data = serializer.toUint8Array();
+    console.log(`run_pool_op: ${this.poolAddr} unblacklist_position`);
+    console.log(`args: `, data);
+  }
+}
+
+export const runPoolOp = async () => {
+  console.log("runPoolOp");
+};
